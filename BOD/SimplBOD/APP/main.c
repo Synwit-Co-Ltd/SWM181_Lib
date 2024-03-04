c7922447b7ca9bb9da13cac96b7a50b5259e08da
XIVN1987 <XIVN1987@163.com>
2024-03-04 10:21:55 +0800

    correct BOD demo

--- BOD/SimplBOD/APP/main.c
+++ BOD/SimplBOD/APP/main.c
@@ -1,11 +1,12 @@
 #include "SWM181.h"
 
+void SerialInit(void);
 
 int main(void)
-{	
+{
 	SystemInit();
 	
-	GPIO_Init(GPIOA, PIN5, 1, 0, 0, 0);			//输出，接LED
+	SerialInit();
 	
 	SYS->BODCR |= (0x01 << SYS_BODCR_IE_Pos);
 	
@@ -13,11 +14,51 @@ int main(void)
 	
 	while(1==1)
 	{
-		GPIO_ClrBit(GPIOA, PIN5);
 	}
 }
 
 void IRQ6_Handler(void)
 {
-	GPIO_SetBit(GPIOA, PIN5);
+	SYS->BODCR |= SYS_BODCR_IF_Msk;
+	
+	printf("BOD Interrupt happened!\n");
+}
+
+
+void SerialInit(void)
+{
+	UART_InitStructure UART_initStruct;
+	
+	PORT_Init(PORTA, PIN0, FUNMUX_UART0_RXD, 1);	//GPIOA.0配置为UART0输入引脚
+	PORT_Init(PORTA, PIN1, FUNMUX_UART0_TXD, 0);	//GPIOA.1配置为UART0输出引脚
+ 	
+ 	UART_initStruct.Baudrate = 57600;
+	UART_initStruct.DataBits = UART_DATA_8BIT;
+	UART_initStruct.Parity = UART_PARITY_NONE;
+	UART_initStruct.StopBits = UART_STOP_1BIT;
+	UART_initStruct.RXThreshold = 3;
+	UART_initStruct.RXThresholdIEn = 0;
+	UART_initStruct.TXThreshold = 3;
+	UART_initStruct.TXThresholdIEn = 0;
+	UART_initStruct.TimeoutTime = 10;
+	UART_initStruct.TimeoutIEn = 0;
+ 	UART_Init(UART0, &UART_initStruct);
+	UART_Open(UART0);
+}
+
+/****************************************************************************************************************************************** 
+* 函数名称: fputc()
+* 功能说明: printf()使用此函数完成实际的串口打印动作
+* 输    入: int ch		要打印的字符
+*			FILE *f		文件句柄
+* 输    出: 无
+* 注意事项: 无
+******************************************************************************************************************************************/
+int fputc(int ch, FILE *f)
+{
+	UART_WriteByte(UART0, ch);
+	
+	while(UART_IsTXBusy(UART0));
+ 	
+	return ch;
 }
